19d174166b6a4bcf738f41c48bf574832308b230
Rid-lin <vlad@vegner.org>
2020-10-23 11:29:24 +0500

    Added: Reading configuration from environment variables or (if not present) from .env files

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/rid-lin/go-template/config"
 )
 
 type configType struct {
@@ -10,25 +15,43 @@ type configType struct {
 }
 
 var (
-	config configType
+	conf configType
 )
 
 func init() {
 
-	flag.IntVar(&config.logLevel, "log-level", 1, `Level log: 
+	flag.IntVar(&conf.logLevel, "log-level", 1, `Level log: 
 		0 - silent, 
 		1 - error, start and end, 
 		2 - error, start and end, warning, 
 		3 - error, start and end, warning, info,
 		4 - error, start and end, warning, info and more info`)
 	flag.Parse()
-	if config.logLevel > 4 {
-		config.logLevel = 4
+	if conf.logLevel > 4 {
+		conf.logLevel = 4
 	}
-	if config.logLevel != 0 {
+	if conf.logLevel != 0 {
 		log.SetFlags(log.Ldate | log.Ltime)
 	} // If logLevel not specified - silent mode
+	if len(os.Args) == 1 {
+		// loads values from .env into the system
+		if err := godotenv.Load(); err != nil {
+			log.Print("No .env file found")
+		}
+	}
 }
 
 func main() {
+	conf := config.New()
+
+	// Print out environment variables
+	fmt.Println(conf.GitHub.Username)
+	fmt.Println(conf.GitHub.APIKey)
+	fmt.Println(conf.DebugMode)
+	fmt.Println(conf.MaxUsers)
+
+	// Print out each role
+	for _, role := range conf.UserRoles {
+		fmt.Println(role)
+	}
 }
